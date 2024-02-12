71de6c85791e4043a06e8207de27c95606c9d233
ThomasCharuel <ThomasCharuel@users.noreply.github.com>
2024-02-12 10:30:33 +0100

    Save

---
+++ module00/ex01/Contact.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tcharuel <tcharuel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/11 21:33:18 by tcharuel          #+#    #+#             */
+/*   Updated: 2024/02/12 10:11:39 by tcharuel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Contact.hpp"
+
+Contact::Contact(void)
+{
+	std::out << "Constructor called" << std::endl;
+}
+
+Contact::~Contact(void)
+{
+	std::out << "Destructor called" << std::endl;
+}
---
+++ module00/ex01/PhoneBook.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tcharuel <tcharuel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/11 21:33:29 by tcharuel          #+#    #+#             */
+/*   Updated: 2024/02/12 10:09:52 by tcharuel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONE_BOOK_HPP
+# define PHONE_BOOK_HPP
+
+class PhoneBook
+{
+  public:
+	PhoneBook(void);
+	~PhoneBook(void);
+};
+
+#endif
