d85b7b6eb90f042d22a2f82395d9447f48b25e39
tcharuel <16272579+ThomasCharuel@users.noreply.github.com>
2024-02-24 19:38:54 +0100

    Implement sed

--- module01/ex04/Sed.cpp
+++ module01/ex04/Sed.cpp
@@ -6,7 +6,7 @@
 /*   By: tcharuel <tcharuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 08:35:15 by tcharuel          #+#    #+#             */
-/*   Updated: 2024/02/24 18:28:35 by tcharuel         ###   ########.fr       */
+/*   Updated: 2024/02/24 19:38:45 by tcharuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,13 +47,28 @@ void Sed::run(void) const
 	char c;
 	std::ifstream ifs(this->_infile.c_str());
 	std::ofstream ofs(this->_outfile.c_str());
+	std::string buffer;
 
-	while (ifs.good())
-	{
+	while (true) {
 		c = ifs.get();
-		if (c)
-			ofs << c;
-	}
+		if (!ifs.good())
+			break;
+		buffer += c;
+		if (!buffer.compare(this->_old_str.substr(0, buffer.length())))
+		{
+			if (buffer == this->_old_str)
+			{
+				ofs << this->_new_str;
+				buffer.clear();
+			}
+		}
+		else
+		{
+			ofs << buffer;
+			buffer.clear();
+		}
+	};
+	ofs << buffer;
 	ifs.close();
 	ofs.close();
 }
